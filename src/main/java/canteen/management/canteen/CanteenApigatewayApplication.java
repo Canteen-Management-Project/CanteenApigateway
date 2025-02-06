2d177e7ab616cda574a1dab38981927623d73c04
ketand <ketand@cdac.in>
2025-02-06 15:05:41 +0530

    Initial Setup of API Gateway

---
+++ src/main/java/canteen/management/canteen/CanteenApigatewayApplication.java
@@ -0,0 +1,13 @@
+package canteen.management.canteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanteenApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanteenApigatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/canteen/management/canteen/CanteenApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package canteen.management.canteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
